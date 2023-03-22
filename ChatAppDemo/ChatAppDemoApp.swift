5505596ec295ae04897ebee162c6bd396e7554a0
Bashar <bashar.albagdadi@gmail.com>
2023-03-22 09:16:52 +0100

    Initial Commit

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by Bashar on 2023-03-22.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
